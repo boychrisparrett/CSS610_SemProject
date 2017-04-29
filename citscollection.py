000bbde0ceb80637a8610182d87e3845659ba4f8
boychrisparrett <cparret2@gmu.edu>
2017-04-29 11:26:30 -0400

    Updated to help with Interface Integration
    Added comments. 
    Added constants that can be set through the interface

--- citscollection.py
+++ citscollection.py
@@ -1,6 +1,7 @@
 ##############################################################################
-# Author: Christopher M. Parrett
-# Homework #2, due 08FEB2017
+# Authors: Christopher M. Parrett, Tom Pike
+# Term Project - Complex Intelligence Preparation of the Battlefield
+#
 # Computational Social Science 610: Agent Based Modeling and Simulation
 # Spring 2017, Department of Computational and Data Sciences,
 # Under the most excellent tutelage of Dr. R Axtell, George Mason Univ
@@ -9,84 +10,94 @@
 # using Python 3.5.2 | Anaconda 4.2.0 (64-bit).
 ##############################################################################
 ##############################################################################
-
 import random
 from cits import *
 from math import sqrt
 from statistics import median
 
-global_cit_ideo_mean = 96.0
-global_cit_ideo_sd = 12
-global_cit_wealth_mean = 6.0
-global_cit_wealth_sd = 13
-global_party_number = 1
-global_talkspan = 10
-
 ##############################################################################
 ##############################################################################
-# CLASS::
+# CLASS::CITS_Collection
 #
-# Purpose:
-#
+# Purpose: Implements the array of CITS turtles
 #
 class CITS_Collection:
-
+    #Constants that will be set through the interface
+    CONST_IDEO_MEAN = 96.0
+    CONST_IDEO_SD = 12
+    CONST_WEALTH_MEAN = 6.0
+    CONST_WEALTH_SD = 13
+    CONST_PARTY_NUMBER = 1
+    CONST_TALKSPAN = 10
+    
+    ########################################################################
+    #Standard initiatlization routine
     def __init__(self):
         #Declare Collection Variables
         self.cits = []
         self.numcits = 0
 
+    ########################################################################
+    #Standard GET/SET routins for individual turtle with UID = idx
+    def getCITS(self): return self.cits        #Returns entire array
+    def getNumCITS(self): return self.numcits
+    
+    ########################################################################
+    #Standard GET/SET routins for individual turtle with UID = idx
     def getCIT(self,idx): return self.cits[idx]
     def setCIT(self,idx,c): self.cits[idx] = c
 
-    def getCITS(self): return self.cits
-    def getNumCITS(self): return self.numcits
-
     ##----------------------------------------------------------------------
-    ## Name:
+    ## Name: Initialize
     ##
     ## Desc:
     ##
     ## Paramters:
-    ##    1)
-    ##    2)
-    ##    3)
+    ##    1) numcits: The number of agents to instantiate
+    ##    2) max_x: The dimension of the cartesian grid in the X direction
+    ##    3) max_y: The dimension of the cartesian grid in the Y direction
     ##
     ## Returns: Nothing
     def Initialize(self,numcits,max_x,max_y):
         #Declare Collection Variables
         self.numcits = numcits
 
+        #Instantiate the Grid Coords, X and Y
         xarr = list(range(max_x))
         yarr = list(range(max_y))
-
+        
+        #Ensure there is room enough for 1 agent per cell
         if numcits > max_x*max_y:
             print("ERROR: Number of CITS cannot exceed number of cells")
             return -1
 
-        #Initiate CITS
+        #Initiate CITS agents
         for i in range(self.numcits):
+            
+            #Pick available random X,Y pair and remove from future use
             ix = random.choice(xarr)
             xarr.remove(ix)
             iy = random.choice(yarr)
             yarr.remove(iy)
 
+            #Instantiate the agent
             c = CITS(i,ix,iy)
             print("CITS #%s created at %s %s"%(i,ix,iy))
+            
+            #Initialize the values
             c.setStakeholder(False)
-            c.setParty(random.randint(0,global_party_number))
+            c.setParty(random.randint(0,self.CONST_PARTY_NUMBER))
 
-            #########################
-            ###!!!CORRECT BELOW!!!###
-            c.setIdeo(random.normalvariate(global_cit_ideo_mean, global_cit_ideo_sd))
+            #set ideo random-normal cit_ideo_mean cit_ideo_sd
+            c.setIdeo(random.normalvariate(self.CONST_IDEO_MEAN, self.CONST_IDEO_SD))
+            
             c.setOwn(Entity.PRF, c.getIdeo())
-            #########################
 
             if random.randint(0,100) <= c.getSelectorate():
                 c.setSelectorate(True)
 
             #set wealth random-normal cit_wealth_mean cit_wealth_sd
-            c.setWealth(random.normalvariate(global_cit_wealth_mean,global_cit_wealth_sd))
+            c.setWealth(random.normalvariate(self.CONST_WEALTH_MEAN,self.CONST_WEALTH_SD))
             print("\twith wealth = %s"%c.getWealth())
             
             if c.getSelectorate():
@@ -94,25 +105,58 @@ class CITS_Collection:
 
             #Add CITS to array
             self.cits.append(c)
-
+    ##----------------------------------------------------------------------
+    ## Name: InitSatisfaction
+    ##
+    ## Desc: Sets satisfaction of all agents
+    ##
+    ## Paramters:
+    ##
+    ## Returns: Nothing     
     def InitSatisfaction(self):
         #ask cits [...
         for c in self.cits:
             c.setSatisfaction(self.__CalcSatisfaction__(c))
-
+            
+    ##----------------------------------------------------------------------
+    ## Name: __CalcSatisfaction__
+    ##
+    ## Desc: PRIVATE function that calculates satisfaction of all agents
+    ##
+    ## Paramters:
+    ##
+    ## Returns: Nothing  
     def __CalcSatisfaction__(self,c):
         #set satisfaction wealth * (wealth / median [wealth] of cits) * (ideo / median [ideo] of cits)
         afact= c.getWealth() * (c.getWealth() / self.GetMedian("wealth"))
         bfact= c.getIdeo() / self.GetMedian("ideo")
         return ( afact * bfact )
 
-
+    ##----------------------------------------------------------------------
+    ## Name: UpdatePower
+    ##
+    ## Desc: Updates the power of all agents
+    ##
+    ## Paramters:
+    ##     1) maxpower: the calculated maximum power of the system
+    ##
+    ## Returns: Nothing  
     def UpdatePower(self,maxpower):
         for c in self.cits:
             c.setPower( c.getRawpower() / maxpower )
             c.setOwn(Entity.POW, c.getPower())
 
-
+    ##----------------------------------------------------------------------
+    ## Name: UpdateSatisfaction
+    ##
+    ## Desc: Updates the power of all agents
+    ##
+    ## Paramters:
+    ##     1) base: The economic base level of a government
+    ##     2) tax: The tax levied by the government
+    ##     3) gov_ideo: the government Ideo
+    ##
+    ## Returns: Nothing  
     def UpdateSatisfaction(self,base,tax,gov_ideo):
         #ask cits [
         for c in self.cits:
@@ -143,61 +187,123 @@ class CITS_Collection:
             #set rawpower wealth
             c.setRawpower( c.getWealth() )
 
-
+    ##----------------------------------------------------------------------
+    ## Name: NodesWithinRange
+    ##
+    ## Desc: Find all nodes with the radius of talkspan 
+    ##
+    ## Paramters:
+    ##    1) orig: Originator node
+    ##
+    ## Returns: array of agent UIDs within TALKSPAN distance
     def NodesWithinRange(self,orig):
+        #Store orginator UID, X and Y coords for calculations
         x1 = orig.getXCor()
         y1 = orig.getYCor()
         uid = orig.getUID()
-
+        #Instantiate array
         ret= []
-        #ask c [
         print("Assessing uid ",uid)
         for c in self.cits:
             #create-linkcits-to cits in-radius talkspan with [who != [who] of myself] [
+            #Assess all OTHER nodes...
             if uid != c.getUID():
+                #Calculate cartesian distance SQRT(X^2+Y^2)
                 dist = sqrt(pow((c.getXCor() - x1),2) + pow((c.getYCor() - y1),2))
                 print("\t against: ",c.getUID(), " with dist = ", dist)
-                if dist <= global_talkspan:
+                if dist <= self.CONST_TALKSPAN:
+                    #Node falls within talkspan distance
                     ret.append(c.getUID())
         return ret
 
-
+    ##----------------------------------------------------------------------
+    ## Name: GetMedian
+    ##
+    ## Desc: Returns median value of the parameter requested
+    ##
+    ## Paramters:
+    ##    1) fld: The parameter name to search/utlilize
+    ##
+    ## Returns: median value 
     def GetMedian(self,fld):
+        #create empty array for values
         med = []
+        
+        #build array of values
         if fld == "wealth":
             for i in self.cits: 
                 med.append(i.getWealth())
         elif fld == "ideo":
             for i in self.cits: 
                 med.append(i.getIdeo())
-        return int(median(med))
-
+        #return median of array of values using the statistics.median function
+        return median(med)
+    
+    ##----------------------------------------------------------------------
+    ## Name: GetSum
+    ##
+    ## Desc: Summarizes the values of the parameter requested
+    ##
+    ## Paramters:
+    ##    1) fld: The parameter name to search/utlilize
+    ##
+    ## Returns: sum of values
     def GetSum(self,fld):
+        #Step through each agent in cits and return sum
         med = 0.0
         if fld == "wealth":
             for i in self.cits: med += i.getWealth()
         elif fld == "ideo":
             for i in self.cits: med += i.getIdeo()
         return med
-
+    
+    ##----------------------------------------------------------------------
+    ## Name: GetMax
+    ##
+    ## Desc: Finds the maximum value of the parameter requested
+    ##
+    ## Paramters:
+    ##    1) fld: The parameter name to search/utlilize
+    ##
+    ## Returns: maximum value of parameter
     def GetMax(self, fld):
+        #All values must be zero or positive
         mx = -1
         if fld == "proximity":
+            #step through each Agent
             for i in self.cits:
+                #find a higher value and save
                 if mx < i.getProximity(): mx = i.getProximity()
         elif fld == "rawpower":
+            #step through each Agent
             for i in self.cits:
+                #find a higher value and save
                 if mx < i.getRawpower(): mx = i.getRawpower()
         return mx
-
+    
+    ##----------------------------------------------------------------------
+    ## Name: GetMin
+    ##
+    ## Desc: Finds the minimum value of the parameter requested
+    ##
+    ## Paramters:
+    ##    1) fld: The parameter name to search/utlilize
+    ##
+    ## Returns: minimum value of parameter
     def GetMin(self, fld):
-        min = 10000000
+        #set a abnormally large number
+        min = 100000000
+        
         if fld == "proximity":
+            #step through each Agent
             for i in self.cits:
                 if min > i.getProximity():
+                    #found a lower value, save
                     min = i.getProximity()
         elif fld == "rawpower":
+            #step through each Agent
             for i in self.cits:
                 if min > i.getRawpower():
+                    #found a lower value, save
                     min = i.getRawpower()
         return min
